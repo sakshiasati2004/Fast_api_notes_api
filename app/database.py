b196df3aef2f75c5704a0374e52eea81bafce85b
sakshiasati2004 <sakshiasati07@gmail.com>
2026-03-05 17:27:37 +0530

    Switched database from SQLite to PostgreSQL

--- app/database.py
+++ app/database.py
@@ -2,13 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./notes.db"
+# PostgreSQL Database URL
+SQLALCHEMY_DATABASE_URL = "postgresql://sakshi:password123@localhost/notes_db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+# Create database engine
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
+# Create session factory
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
+# Base class for models
 Base = declarative_base()
